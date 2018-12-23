13812ad0b67c664f2dcfa2e20ed49abe3dea1f5b
Elbert <elbert.bae@gmail.com>
2018-12-23 22:41:27 +0000

    blogscript.js file gets top 5 lastest posts

---
+++ blogscript.js
@@ -0,0 +1,29 @@
+#!/bin/dash
+require('dotenv').config();
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DBNAME,
+  port: process.env.DBPORT,
+})
+
+pool.query('SELECT * FROM posts ORDER BY date DESC LIMIT 5')
+  .then((result) => {
+    console.log("This result is based on the date (4th column).")
+    console.log(result.rows);
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+
+pool.query('SELECT * FROM posts LIMIT 5')
+  .then((result) => {
+    console.log("This result is based on the last 5 entries (no ORDER BY).")
+    console.log(result.rows);
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+
+pool.end();
--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.get('/', (request, response) => {
   response.render('url_home');
 })
 
+// 'CREATE DATABASE <name>'
+// 'CREATE TABLE posts(title VARCHAR(255), description VARCHAR, email VARCHAR, date VARCHAR)'
+
 app.get('/posts', (request, response) => {
   pool.query('SELECT * FROM posts ORDER BY date DESC')
     .then((res) => {
@@ -36,25 +39,33 @@ app.get('/posts/submit', (request, response) => {
   response.render('url_submit');
 })
 
+// input to post (pg)
 app.post('/posts/submit', (request, response) => {
   const input = request.body;
 
   // TODO: apply proper error handling for UI
-  if (input.title.length < 255) {
+  if (input.title.length > 255) {
+    console.log('Title cannot be longer than 255 characters.')
+    return;
+  }
+  if (!input.title) {
+    console.log('Title cannot be empty.')
     return;
   }
   if (input.description.length <= 1000 && input.description.length < 3) {
+    console.log('Content must be between 3 to 1000 characters!')
     return;
+  } else {
+    const date = (new Date).toDateString();
+    pool.query('INSERT INTO posts(title, description, email, date) VALUES ($1, $2, $3, $4);', 
+      [input.email, input.title, input.description, date])
+      .then((result) => {;
+        response.redirect('/posts');
+      })
+      .catch((err) => {
+        console.error(err);
+      })
   }
-  const date = (new Date).toDateString();
-  pool.query('INSERT INTO posts(title, description, email, date) VALUES ($1, $2, $3, $4);', 
-    [input.email, input.title, input.description, date])
-    .then((result) => {;
-      response.redirect('/posts');
-    })
-    .catch((err) => {
-      console.error(err);
-    })
 })
 
 app.listen(PORT, () => {
